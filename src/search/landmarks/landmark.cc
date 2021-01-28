c188fa6d45de8bb74b4b52a478490e1edd3ae882
ClemensBuechner <clemens.buechner@unibas.ch>
2021-01-28 16:26:18 +0100

    [issue999] Implement functions true_in_state.

--- src/search/landmarks/landmark.cc
+++ src/search/landmarks/landmark.cc
@@ -1,1 +1,53 @@
 #include "landmark.h"
+
+#include <algorithm>
+
+using namespace std;
+
+namespace landmarks {
+bool AtomicFactLandmark::is_true_in_state(
+    const GlobalState &state) const {
+
+    return state[fact.var] == fact.value;
+}
+
+bool AtomicFactLandmark::is_true_in_state(const State &state) const {
+    return state[fact.var].get_value() == fact.value;
+}
+
+bool DisjunctiveFactLandmark::is_true_in_state(
+    const GlobalState &state) const {
+
+    return any_of(facts.begin(), facts.end(),
+                  [state](const FactPair &fact) {
+                      return state[fact.var] == fact.value;
+                  });
+}
+
+bool DisjunctiveFactLandmark::is_true_in_state(
+    const State &state) const {
+
+    return any_of(facts.begin(), facts.end(),
+                  [state](const FactPair &fact) {
+                      return state[fact.var].get_value() == fact.value;
+                  });
+}
+
+bool ConjunctiveFactLandmark::is_true_in_state(
+    const GlobalState &state) const {
+
+    return all_of(facts.begin(), facts.end(),
+                  [state](const FactPair &fact) {
+                      return state[fact.var] == fact.value;
+                  });
+}
+
+bool ConjunctiveFactLandmark::is_true_in_state(
+    const State &state) const {
+
+    return all_of(facts.begin(), facts.end(),
+                  [state](const FactPair &fact) {
+                      return state[fact.var].get_value() == fact.value;
+                  });
+}
+}
--- src/search/landmarks/landmark.h
+++ src/search/landmarks/landmark.h
@@ -1,14 +1,41 @@
 #ifndef LANDMARKS_LANDMARK_H
 #define LANDMARKS_LANDMARK_H
 
-#include "../global_state.h"
+#include "../task_proxy.h"
+
+#include <set>
 
 namespace landmarks {
 class Landmark {
 public:
     virtual ~Landmark() = default;
+};
+
+class FactLandmark : public Landmark {
+public:
     virtual bool is_true_in_state(const GlobalState &state) const = 0;
     virtual bool is_true_in_state(const State &state) const = 0;
+};
+
+class AtomicFactLandmark : public FactLandmark {
+    FactPair &fact;
+public:
+    bool is_true_in_state(const GlobalState &state) const override;
+    bool is_true_in_state(const State &state) const override;
+};
+
+class DisjunctiveFactLandmark : public FactLandmark {
+    std::vector<FactPair> facts;
+public:
+    virtual bool is_true_in_state(const GlobalState &state) const override;
+    virtual bool is_true_in_state(const State &state) const override;
+};
+
+class ConjunctiveFactLandmark : public FactLandmark {
+    std::vector<FactPair> facts;
+public:
+    virtual bool is_true_in_state(const GlobalState &state) const override;
+    virtual bool is_true_in_state(const State &state) const override;
 };
 }
 
