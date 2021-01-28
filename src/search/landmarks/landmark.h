983bd7f17bd935158191442242135ad996952d2f
ClemensBuechner <clemens.buechner@unibas.ch>
2021-01-28 15:47:18 +0100

    [issue999] Add virtual functions to check if true in state.

--- src/search/landmarks/landmark.h
+++ src/search/landmarks/landmark.h
@@ -1,8 +1,15 @@
 #ifndef LANDMARKS_LANDMARK_H
 #define LANDMARKS_LANDMARK_H
 
-class landmark {
+#include "../global_state.h"
 
+namespace landmarks {
+class Landmark {
+public:
+    virtual ~Landmark() = default;
+    virtual bool is_true_in_state(const GlobalState &state) const = 0;
+    virtual bool is_true_in_state(const State &state) const = 0;
 };
+}
 
 #endif
