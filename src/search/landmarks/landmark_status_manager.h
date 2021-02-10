006ad1b14fc5561fbbe0fe61516678dc26c14e9a
ClemensBuechner <clemens.buechner@unibas.ch>
2021-02-10 19:49:32 +0100

    [issue1000] Change import.

--- src/search/landmarks/landmark_status_manager.h
+++ src/search/landmarks/landmark_status_manager.h
@@ -1,8 +1,9 @@
 #ifndef LANDMARKS_LANDMARK_STATUS_MANAGER_H
 #define LANDMARKS_LANDMARK_STATUS_MANAGER_H
 
+#include "landmark_graph.h"
+
 #include "../per_state_bitset.h"
-#include "landmark_count_heuristic.h"
 
 namespace landmarks {
 class LandmarkGraph;
