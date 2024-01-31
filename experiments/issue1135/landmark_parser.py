016dea92c41b48871d6feb713de1638d6e8c89ad
ClemensBuechner <clemens.buechner@unibas.ch>
2024-01-31 15:43:51 +0100

    Add experiment issue1135.

---
+++ experiments/issue1135/landmark_parser.py
@@ -0,0 +1,20 @@
+import re
+
+from lab.parser import Parser
+
+PATTERNS = [
+    ["lmgraph_generation_time", r"Landmark graph generation time: (.+)s", float],
+    ["landmarks", r"Landmark graph contains (\d+) landmarks, of which \d+ are disjunctive, \d+ are conjunctive, and \d+ are derived.", int],
+    ["landmarks_disjunctive", r"Landmark graph contains \d+ landmarks, of which (\d+) are disjunctive, \d+ are conjunctive, and \d+ are derived.", int],
+    ["landmarks_conjunctive", r"Landmark graph contains \d+ landmarks, of which \d+ are disjunctive, (\d+) are conjunctive, and \d+ are derived.", int],
+    ["landmarks_derived", r"Landmark graph contains \d+ landmarks, of which \d+ are disjunctive, \d+ are conjunctive, and (\d+) are derived.", int],
+    ["orderings", r"Landmark graph contains (\d+) orderings.", int],
+    ["landmarks_dominance_pruned", r"Pruned (\d+) dominated landmarks.", int],
+    ["orderings_deduced", r"Deduced (\d+) orderings.", int],
+]
+
+class LandmarkParser(Parser):
+    def __init__(self):
+        Parser.__init__(self)
+        for name, pattern, typ in PATTERNS:
+            self.add_pattern(name, pattern, type=typ)
