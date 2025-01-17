fac70bf95213985128846d5e93cf2fd315acec58
ClemensBuechner <clemens.buechner@unibas.ch>
2025-01-17 15:54:49 +0100

    Filter unsupported tasks in experiments.

--- experiments/issue1074/v1-opt.py
+++ experiments/issue1074/v1-opt.py
@@ -74,8 +74,21 @@ $ echo $LD_LIBRARY_PATH
 """
 # setup='export PATH=/scicore/soft/apps/binutils/2.32-GCCcore-8.3.0/bin:/scicore/soft/apps/CMake/3.15.3-GCCcore-8.3.0/bin:/scicore/soft/apps/cURL/7.66.0-GCCcore-8.3.0/bin:/scicore/soft/apps/bzip2/1.0.8-GCCcore-8.3.0/bin:/scicore/soft/apps/ncurses/6.1-GCCcore-8.3.0/bin:/scicore/soft/apps/GCCcore/8.3.0/bin:/infai/sieverss/repos/bin:/infai/sieverss/local:/export/soft/lua_lmod/centos7/lmod/lmod/libexec:/usr/local/bin:/usr/bin:/usr/local/sbin:/usr/sbin:$PATH\nexport LD_LIBRARY_PATH=/scicore/soft/apps/binutils/2.32-GCCcore-8.3.0/lib:/scicore/soft/apps/cURL/7.66.0-GCCcore-8.3.0/lib:/scicore/soft/apps/bzip2/1.0.8-GCCcore-8.3.0/lib:/scicore/soft/apps/zlib/1.2.11-GCCcore-8.3.0/lib:/scicore/soft/apps/ncurses/6.1-GCCcore-8.3.0/lib:/scicore/soft/apps/GCCcore/8.3.0/lib64:/scicore/soft/apps/GCCcore/8.3.0/lib'
 
+UNSUPPORTED = []
+def collect_unsupported(run):
+    if run["error"] == "search-unsupported":
+        UNSUPPORTED.append(
+            (run["algorithm"].split(":")[0], run["domain"], run["problem"]))
+    return True
+
+def filter_unsupported(run):
+    for algo, domain, problem in UNSUPPORTED:
+        if algo in run["algorithm"] and domain == run["domain"] and problem == run["problem"]:
+            return False
+    return True
+
 if common_setup.is_test_run():
-    SUITE = IssueExperiment.DEFAULT_TEST_SUITE
+    SUITE = IssueExperiment.DEFAULT_TEST_SUITE + ["assembly:prob01.pddl"]
     ENVIRONMENT = LocalEnvironment(processes=4)
 
 exp = IssueExperiment(
@@ -113,7 +126,8 @@ def make_comparison_tables():
         name = f"seq-opt-bjolp-{cp}"
         compared_configs.append(
             (f"{rev}-{name}", f"{rev}-{name}:only-causal", f"Diff ({name})"))
-    report = ComparativeReport(compared_configs, attributes=ATTRIBUTES)
+    report = ComparativeReport(compared_configs, attributes=ATTRIBUTES,
+                               filter=[collect_unsupported, filter_unsupported])
     outfile = os.path.join(
         exp.eval_dir,
         f"{exp.name}-only-causal-compare.{report.output_format}")
