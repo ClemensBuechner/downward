7f712bccc154624d48c403e7593fa88788a7601c
ClemensBuechner <clemens.buechner@unibas.ch>
2025-04-28 14:09:53 +0200

    Revert requirement of C++ concepts.

--- src/search/landmarks/util.cc
+++ src/search/landmarks/util.cc
@@ -31,11 +31,13 @@ bool possibly_reaches_landmark(const OperatorProxy &op,
     }
 
     // Check whether an effect of `op` reaches an atom in `landmark`.
-    EffectsProxy effects = op.get_effects();
-    return any_of(begin(effects), end(effects), [&](const EffectProxy &effect) {
-                      return landmark.contains(effect.get_fact().get_pair()) &&
-                      condition_is_reachable(effect.get_conditions(), reached);
-                  });
+    for (const EffectProxy &effect : op.get_effects()) {
+        if (landmark.contains(effect.get_fact().get_pair()) &&
+            condition_is_reachable(effect.get_conditions(), reached)) {
+            return true;
+        }
+    }
+    return false;
 }
 
 utils::HashSet<FactPair> get_intersection(
